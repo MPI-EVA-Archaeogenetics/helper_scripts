a6a4bef3708e283c9bfbd16d72c03a5b7f18a1db
gagr88 <92851876+gagr88@users.noreply.github.com>
2023-05-25 12:41:57 +0200

    Update samples_eager_status/samples_eager_status.sh
    Co-authored-by: Thiseas C. Lamnidis <thisseass@gmail.com>

--- samples_eager_status/samples_eager_status.sh
+++ samples_eager_status/samples_eager_status.sh
@@ -47,6 +47,6 @@ while read line; do
 	num=($(grep -n ${iid} /mnt/archgen/Autorun_eager/*_Autorun_eager_queue.txt | cut -f 1 -d " " | sed s/":cd"/""/g | rev | cut -f 1 -d ":" | rev | tail -1))
 	run=($(grep -n ${iid} /mnt/archgen/Autorun_eager/*_Autorun_eager_queue.txt | cut -f 1 -d " " | sed s/":cd"/""/g | rev | cut -f 2- -d ":" | cut -f 1 -d "/" | rev | tail -1))
 	status=($(exec 2>&- grep "Pipeline completed" /mnt/archgen/Autorun_eager/array_Logs/${run}/AE_spawner*txt.o*.${num} | awk '{OFS = "-"; print $2,$3,$4}'))
-	echo -e ${iid}"\t"${status} >> ${inf}_samples_eager_status.txt
+	echo -e ${iid}"\t"${status}
 done < <(cat ${inf})
 
