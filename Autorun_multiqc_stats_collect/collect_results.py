4ae075da68adbdce47128a77e3692c2acdad92cd
Thiseas Christos Lamnidis <thisseass@gmail.com>
2023-11-13 16:06:37 +0100

    Added column name standardisation

--- Autorun_multiqc_stats_collect/collect_results.py
+++ Autorun_multiqc_stats_collect/collect_results.py
@@ -18,10 +18,31 @@ def get_individual_library_stats(mqc_data):
     for key in data["report_saved_raw_data"]["multiqc_general_stats"].keys():
         ## If the key contains a '.', it is a library stat, otherwise it is a sample stat
         if len(key.split(".")) > 1:
-            library_stats[key] = data["report_saved_raw_data"]["multiqc_general_stats"][
-                key
-            ]
-            sample_libraries.append(key)  ## Keep track of library IDs for later
+            ## eager 2.5.0 also has split by UDG for some stats. we want to compile these together, as each library can only have one udg treatment.
+            ## By splitting by '_udg', we can get the library ID, and then add the attributes to the library dict for both cases.
+            try:
+                ## If the library key is already in the dict, add the attributes to it
+                library_stats[key.split("_udg")[0]].update(
+                    data["report_saved_raw_data"]["multiqc_general_stats"][key]
+                )
+            except KeyError:
+                ## If the library ID doesn't exist in the dict, create it
+                library_stats[key.split("_udg")[0]] = data["report_saved_raw_data"][
+                    "multiqc_general_stats"
+                ][key]
+                sample_libraries.append(
+                    key
+                )  ## Keep track of library IDs for later. Only add it if its new.
+            ## Not actually needed since key.split("_udg")[0] will always be the Library_ID
+            # else:
+            #     ## Library key is actual Library_ID
+            #     try:
+            #         ## If the library key is already in the dict, add the attributes to it
+            #         library_stats[key].update(data["report_saved_raw_data"]["multiqc_general_stats"][key])
+            #     except KeyError:
+            #         ## If the library ID doesn't exist in the dict, create it
+            #         library_stats[key] = data["report_saved_raw_data"]["multiqc_general_stats"][key]
+            #     sample_libraries.append(key)  ## Keep track of library IDs for later
         else:
             sample_stats[key] = data["report_saved_raw_data"]["multiqc_general_stats"][
                 key
@@ -38,8 +59,120 @@ def get_individual_library_stats(mqc_data):
         ## Use union of attributes to combine dicts. attributes from the library level will overwrite any that exist in the sample level. Should be no overlap, but good to note.
         # results[library] = dict(sample_stats[library.split('.')[0]] | library_stats[library])
 
+    ## Standardise column naming across multiqc versions
+    results = standardise_column_names(results)
     ## results is a dict of dicts, with the library ID as the key. The value then contains a dict of the combined stats for that library/sample.
     return results
+
+
+## Some column names changed from 2.4.5 to 2.5.0, so we need to standardise the names of these columns across both versions.
+def standardise_column_names(collected_stats):
+    #    OLD NAME                                                                               NEW NAME
+    #    DamageProfiler_mqc-generalstats-damageprofiler-3_Prime1                                mapDamage_mqc-generalstats-mapdamage-mapdamage_3_Prime1
+    #    DamageProfiler_mqc-generalstats-damageprofiler-3_Prime2                                mapDamage_mqc-generalstats-mapdamage-mapdamage_3_Prime2
+    #    DamageProfiler_mqc-generalstats-damageprofiler-5_Prime1                                mapDamage_mqc-generalstats-mapdamage-mapdamage_5_Prime1
+    #    DamageProfiler_mqc-generalstats-damageprofiler-5_Prime2                                mapDamage_mqc-generalstats-mapdamage-mapdamage_5_Prime2
+    #    DamageProfiler_mqc-generalstats-damageprofiler-mean_readlength                         NA
+    #    DamageProfiler_mqc-generalstats-damageprofiler-median                                  NA
+    #    DamageProfiler_mqc-generalstats-damageprofiler-std                                     NA
+    #    endorSpy_mqc-generalstats-endorspy-endogenous_dna                                      base endorSpy_mqc-generalstats-base_endorspy-endogenous_dna
+    #    endorSpy_mqc-generalstats-endorspy-endogenous_dna_post                                 base endorSpy_mqc-generalstats-base_endorspy-endogenous_dna_post
+    #    nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_ML_SE             base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method1_ML_SE
+    #    nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_ML_estimate       base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method1_ML_estimate
+    #    nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_MOM_SE            base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method1_MOM_SE
+    #    nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_MOM_estimate      base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method1_MOM_estimate
+    #    nuclear_contamination_mqc-generalstats-nuclear_contamination-Method2_ML_SE             base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method2_ML_SE
+    #    nuclear_contamination_mqc-generalstats-nuclear_contamination-Method2_ML_estimate       base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method2_ML_estimate
+    #    nuclear_contamination_mqc-generalstats-nuclear_contamination-Method2_MOM_SE            base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method2_MOM_SE
+    #    nuclear_contamination_mqc-generalstats-nuclear_contamination-Method2_MOM_estimate      base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method2_MOM_estimate
+    #    nuclear_contamination_mqc-generalstats-nuclear_contamination-Num_SNPs                  base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Num_SNPs
+    #    snp_coverage_mqc-generalstats-snp_coverage-Covered_Snps                                base snp_coverage_mqc-generalstats-base_snp_coverage-Covered_Snps
+    #    snp_coverage_mqc-generalstats-snp_coverage-Total_Snps                                  base snp_coverage_mqc-generalstats-base_snp_coverage-Total_Snps
+    new_attributes = {
+        "dmg_3p_1": "N/A",
+        "dmg_3p_2": "N/A",
+        "dmg_5p_1": "N/A",
+        "dmg_5p_2": "N/A",
+        "mean_read_length": "N/A",
+        "median_read_length": "N/A",
+        "read_length_std_dev": "N/A",
+        "endogenous": "N/A",
+        "endogenous_post": "N/A",
+        "nuc_cont_m1_ml_se": "N/A",
+        "nuc_cont_m1_ml_est": "N/A",
+        "nuc_cont_m1_mom_se": "N/A",
+        "nuc_cont_m1_mom_est": "N/A",
+        "nuc_cont_m2_ml_se": "N/A",
+        "nuc_cont_m2_ml_est": "N/A",
+        "nuc_cont_m2_mom_se": "N/A",
+        "nuc_cont_m2_mom_est": "N/A",
+        "nuc_cont_snps": "N/A",
+        "snps_covered": "N/A",
+        "snps_total": "N/A",
+    }
+
+    old_names = {
+        "dmg_3p_1": "DamageProfiler_mqc-generalstats-damageprofiler-3_Prime1",
+        "dmg_3p_2": "DamageProfiler_mqc-generalstats-damageprofiler-3_Prime2",
+        "dmg_5p_1": "DamageProfiler_mqc-generalstats-damageprofiler-5_Prime1",
+        "dmg_5p_2": "DamageProfiler_mqc-generalstats-damageprofiler-5_Prime2",
+        "mean_read_length": "DamageProfiler_mqc-generalstats-damageprofiler-mean_readlength",
+        "median_read_length": "DamageProfiler_mqc-generalstats-damageprofiler-median",
+        "read_length_std_dev": "DamageProfiler_mqc-generalstats-damageprofiler-std",
+        "endogenous": "endorSpy_mqc-generalstats-endorspy-endogenous_dna",
+        "endogenous_post": "endorSpy_mqc-generalstats-endorspy-endogenous_dna_post",
+        "nuc_cont_m1_ml_se": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_ML_SE",
+        "nuc_cont_m1_ml_est": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_ML_estimate",
+        "nuc_cont_m1_mom_se": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_MOM_SE",
+        "nuc_cont_m1_mom_est": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_MOM_estimate",
+        "nuc_cont_m2_ml_se": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method2_ML_SE",
+        "nuc_cont_m2_ml_est": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method2_ML_estimate",
+        "nuc_cont_m2_mom_se": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method2_MOM_SE",
+        "nuc_cont_m2_mom_est": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method2_MOM_estimate",
+        "nuc_cont_snps": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Num_SNPs",
+        "snps_covered": "snp_coverage_mqc-generalstats-snp_coverage-Covered_Snps",
+        "snps_total": "snp_coverage_mqc-generalstats-snp_coverage-Total_Snps",
+    }
+
+    new_names = {
+        "dmg_3p_1": "mapDamage_mqc-generalstats-mapdamage-mapdamage_3_Prime1",
+        "dmg_3p_2": "mapDamage_mqc-generalstats-mapdamage-mapdamage_3_Prime2",
+        "dmg_5p_1": "mapDamage_mqc-generalstats-mapdamage-mapdamage_5_Prime1",
+        "dmg_5p_2": "mapDamage_mqc-generalstats-mapdamage-mapdamage_5_Prime2",
+        "endogenous": "base endorSpy_mqc-generalstats-base_endorspy-endogenous_dna",
+        "endogenous_post": "base endorSpy_mqc-generalstats-base_endorspy-endogenous_dna_post",
+        "nuc_cont_m1_ml_se": "base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method1_ML_SE",
+        "nuc_cont_m1_ml_est": "base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method1_ML_estimate",
+        "nuc_cont_m1_mom_se": "base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method1_MOM_SE",
+        "nuc_cont_m1_mom_est": "base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method1_MOM_estimate",
+        "nuc_cont_m2_ml_se": "base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method2_ML_SE",
+        "nuc_cont_m2_ml_est": "base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method2_ML_estimate",
+        "nuc_cont_m2_mom_se": "base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method2_MOM_SE",
+        "nuc_cont_m2_mom_est": "base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Method2_MOM_estimate",
+        "nuc_cont_snps": "base nuclear_contamination_mqc-generalstats-base_nuclear_contamination-Num_SNPs",
+        "snps_covered": "base snp_coverage_mqc-generalstats-base_snp_coverage-Covered_Snps",
+        "snps_total": "base snp_coverage_mqc-generalstats-base_snp_coverage-Total_Snps",
+    }
+
+    ## Starting with all NAs, add in any values that exist in either the old dict or the new dict
+    for library in collected_stats:
+        new_stats = collected_stats[library]
+        new_stats.update(new_attributes)  ## Add all new attributed with NAs
+        ## Deal with older versions of multiqc
+        try:
+            for name, old_name in old_names.items():
+                new_stats[name] = new_stats[old_name]
+        except KeyError:
+            pass
+        ## Deal with newer versions of multiqc
+        try:
+            for name, new_name in new_names.items():
+                new_stats[name] = new_stats[new_name]
+        except KeyError:
+            pass
+        print("library:", library, "stats", new_stats, sep="\n")
+        collected_stats[library] = new_stats
+    return collected_stats
 
 
 ## Hard-coded values
@@ -97,21 +230,21 @@ root_output_path = "/mnt/archgen/Autorun_eager/eager_outputs/"
 
 ## Column order same as old script.
 output_columns = {
-    "Covered_SNPs_on_1240K": "snp_coverage_mqc-generalstats-snp_coverage-Covered_Snps",
-    "Total_SNPs_on_1240K": "snp_coverage_mqc-generalstats-snp_coverage-Total_Snps",
+    "Covered_SNPs_on_1240K": "snps_covered",
+    "Total_SNPs_on_1240K": "snps_total",
     "Nr_of_input_reads": "Samtools Flagstat (pre-samtools filter)_mqc-generalstats-samtools_flagstat_pre_samtools_filter-flagstat_total",
     "Nr_of_mapped_reads": "Samtools Flagstat (pre-samtools filter)_mqc-generalstats-samtools_flagstat_pre_samtools_filter-mapped_passed",
     "Nr_of_input_reads_over_30bp": "Samtools Flagstat (post-samtools filter)_mqc-generalstats-samtools_flagstat_post_samtools_filter-flagstat_total",
-    "%_Endogenous_DNA": "endorSpy_mqc-generalstats-endorspy-endogenous_dna",
+    "%_Endogenous_DNA": "endogenous",
     "Nr_of_mapped_reads_over_30bp": "Samtools Flagstat (post-samtools filter)_mqc-generalstats-samtools_flagstat_post_samtools_filter-mapped_passed",
-    "%_Endogenous_DNA_over_30bp": "endorSpy_mqc-generalstats-endorspy-endogenous_dna_post",
+    "%_Endogenous_DNA_over_30bp": "endogenous_post",
     "Proportion_of_duplicate_reads": "Picard_mqc-generalstats-picard-PERCENT_DUPLICATION",
-    "Damage_5'_bp1": "DamageProfiler_mqc-generalstats-damageprofiler-5_Prime1",
-    "Damage_5'_bp2": "DamageProfiler_mqc-generalstats-damageprofiler-5_Prime2",
-    "Damage_3'_bp1": "DamageProfiler_mqc-generalstats-damageprofiler-3_Prime1",
-    "Damage_3'_bp2": "DamageProfiler_mqc-generalstats-damageprofiler-3_Prime2",
-    "Mean_read_length": "DamageProfiler_mqc-generalstats-damageprofiler-mean_readlength",
-    "Median_read_length": "DamageProfiler_mqc-generalstats-damageprofiler-median",
+    "Damage_5'_bp1": "dmg_5p_1",
+    "Damage_5'_bp2": "dmg_5p_2",
+    "Damage_3'_bp1": "dmg_3p_1",
+    "Damage_3'_bp2": "dmg_3p_2",
+    "Mean_read_length": "mean_read_length",
+    "Median_read_length": "median_read_length",
     "Nr_mtDNA_reads": "mtnucratio_mqc-generalstats-mtnucratio-mtreads",
     "Mean_mt_coverage": "mtnucratio_mqc-generalstats-mtnucratio-mt_cov_avg",
     "mt_to_nuclear_read_ratio": "mtnucratio_mqc-generalstats-mtnucratio-mt_nuc_ratio",
@@ -124,7 +257,7 @@ output_columns = {
     "%_of_genome_covered_by_at_least_4_reads": "QualiMap_mqc-generalstats-qualimap-4_x_pc",
     "%_of_genome_covered_by_at_least_5_reads": "QualiMap_mqc-generalstats-qualimap-5_x_pc",
     "%_GC_of_unique_reads": "QualiMap_mqc-generalstats-qualimap-avg_gc",
-    "Read_length_std_dev": "DamageProfiler_mqc-generalstats-damageprofiler-std",
+    "Read_length_std_dev": "read_length_std_dev",
     "Mean_fold_coverage_on_nuclear_genome.": "mtnucratio_mqc-generalstats-mtnucratio-nuc_cov_avg",
     "Nr_nuclearDNA_reads": "mtnucratio_mqc-generalstats-mtnucratio-nucreads",
     "%_of_mapped_reads": "QualiMap_mqc-generalstats-qualimap-percentage_aligned",
@@ -134,15 +267,15 @@ output_columns = {
     "StdErr_of_Y_relative_coverage": "SexDetErrmine_mqc-generalstats-sexdeterrmine-RateErrY",
     "Relative_coverage_on_X_chromosome": "SexDetErrmine_mqc-generalstats-sexdeterrmine-RateX",
     "Relative_coverage_on_Y_chromosome": "SexDetErrmine_mqc-generalstats-sexdeterrmine-RateY",
-    "Nr_SNPs_used_in_contamination_estimation": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Num_SNPs",
-    "Nuclear_contamination_M1_MOM": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_MOM_estimate",
-    "Nuclear_contamination_M1_MOM_Error": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_MOM_SE",
-    "Nuclear_contamination_M1_ML": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_ML_estimate",
-    "Nuclear_contamination_M1_ML_Error": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_ML_SE",
-    "Nuclear_contamination_M2_MOM": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method2_MOM_estimate",
-    "Nuclear_contamination_M2_MOM_Error": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method2_MOM_SE",
-    "Nuclear_contamination_M2_ML": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method2_ML_estimate",
-    "Nuclear_contamination_M2_ML_Error": "nuclear_contamination_mqc-generalstats-nuclear_contamination-Method2_ML_SE",
+    "Nr_SNPs_used_in_contamination_estimation": "nuc_cont_snps",
+    "Nuclear_contamination_M1_ML": "nuc_cont_m1_ml_est",
+    "Nuclear_contamination_M1_ML_Error": "nuc_cont_m1_ml_se",
+    "Nuclear_contamination_M1_MOM": "nuc_cont_m1_mom_est",
+    "Nuclear_contamination_M1_MOM_Error": "nuc_cont_m1_mom_se",
+    "Nuclear_contamination_M2_ML": "nuc_cont_m2_ml_est",
+    "Nuclear_contamination_M2_ML_Error": "nuc_cont_m2_ml_se",
+    "Nuclear_contamination_M2_MOM": "nuc_cont_m2_mom_est",
+    "Nuclear_contamination_M2_MOM_Error": "nuc_cont_m2_mom_se",
 }
 
 parser = argparse.ArgumentParser(
