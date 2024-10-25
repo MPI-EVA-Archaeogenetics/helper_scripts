5e1a60cd72d9bab2f277faff0960aeb372beb552
Thiseas Christos Lamnidis <thisseass@gmail.com>
2024-10-25 10:21:40 +0200

    Throw exceptions when the functions cannot work, instead of returning None

--- py_helpers/pyPandoraHelper/pyPandoraHelper.py
+++ py_helpers/pyPandoraHelper/pyPandoraHelper.py
@@ -1,3 +1,5 @@
+import sys
+
 def get_site_id(id: str) -> str:
   '''
   This function takes any Pandora_ID and returns the part of it that corresponds to the Site_ID.
@@ -18,6 +20,8 @@ def get_sample_id(id: str) -> str:
   This function takes any Pandora_ID and returns the part of it that corresponds to the Sample_ID.
   '''
   x=id.strip().split('.')
+  if len(x) < 2:
+    raise ValueError('The provided Pandora_ID does not contain the Sample_ID.')
   result=x[0]+'.'+x[1][0]
   return(result)
 
@@ -26,6 +30,8 @@ def get_extract_id(id: str) -> str:
   This function takes any Pandora_ID and returns the part of it that corresponds to the Extract_ID.
   '''
   x=id.strip().split('.')
+  if len(x) < 2:
+    raise ValueError('The provided Pandora_ID does not contain the Extract_ID.')
   result=x[0]+'.'+x[1][0:3]
   return(result)
 
@@ -34,6 +40,8 @@ def get_library_id(id: str) -> str:
   This function takes any Pandora_ID and returns the part of it that corresponds to the Library_ID.
   '''
   x=id.strip().split('.')
+  if len(x) < 2:
+    raise ValueError('The provided Pandora_ID does not contain the Library_ID.')
   result=x[0]+'.'+x[1]
   return(result)
 
@@ -42,6 +50,8 @@ def get_capture_id(id: str) -> str:
   This function takes any Pandora_ID and returns the part of it that corresponds to the Capture_ID.
   '''
   x=id.strip().split('.')
+  if len(x) < 3:
+    raise ValueError('The provided Pandora_ID does not contain the Capture_ID.')
   result=x[0]+'.'+x[1]+'.'+x[2]
   return(result)
 
@@ -50,6 +60,8 @@ def get_sequencing_id(id: str) -> str:
   This function takes any Pandora_ID and returns the part of it that corresponds to the Sequencing_ID.
   '''
   x=id.strip().split('.')
+  if len(x) < 3:
+    raise ValueError('The provided Pandora_ID does not contain the Sequencing_ID.')
   result=x[0]+'.'+x[1]+'.'+x[2]+'.'+x[3]
   return(result)
 
@@ -67,16 +79,21 @@ def main():
   print('Running test cases...')
   print('')
   
-  for id in ["ABC001.A0101.SG1.1", "ABCDE001.A0101.SG1.1"]:
-    print('Pandora_ID:',id)
-    print('Site_ID:',get_site_id(id))
-    print('Individual_ID:',get_ind_id(id))
-    print('Sample_ID:',get_sample_id(id))
-    print('Extract_ID:',get_extract_id(id))
-    print('Library_ID:',get_library_id(id))
-    print('Capture_ID:',get_capture_id(id))
-    print('Sequencing_ID:',get_sequencing_id(id))
-    print('')
+  for id in ["ABC001", "ABC001.A0101", "ABC001.A0101.SG1.1", "ABCDE001.A0101.SG1.1"]:
+    try:
+      print('Pandora_ID:',id)
+      print('Site_ID:',get_site_id(id))
+      print('Individual_ID:',get_ind_id(id))
+      print('Sample_ID:',get_sample_id(id))
+      print('Extract_ID:',get_extract_id(id))
+      print('Library_ID:',get_library_id(id))
+      print('Capture_ID:',get_capture_id(id))
+      print('Sequencing_ID:',get_sequencing_id(id))
+      print('')
+    except ValueError as e:
+      print(e)
+      print('')
+      pass
 
 if __name__ == '__main__':
   main()
