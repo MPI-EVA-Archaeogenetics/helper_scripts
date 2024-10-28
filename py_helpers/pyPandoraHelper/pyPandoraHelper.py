8edeb1f9dc0d1da1d99fca7e0e1401b27f682048
Thiseas Christos Lamnidis <thisseass@gmail.com>
2024-10-28 16:07:30 +0100

    add option to remove/keep _ss suffix (for AE). More complete tests

--- py_helpers/pyPandoraHelper/pyPandoraHelper.py
+++ py_helpers/pyPandoraHelper/pyPandoraHelper.py
@@ -1,12 +1,16 @@
 VERSION = '0.1.0' 
 
-def _remove_suffix(ind_id: str) -> str:
+def _remove_suffix(ind_id: str, keep_ss_suffix: bool = False) -> str:
   '''
   This function takes an Individual_ID and removes the '_ss' suffix if present.
   This suffix is added by Autorun_eager to denote single stranded libraries, and does not appear anywhere in Pandora.
   '''
   if ind_id.strip()[-3:] == '_ss':
-    result=ind_id.strip()[0:-3]
+    ## This is only here as it makes it easier to implement the functionality to keep the suffix downstream.
+    if keep_ss_suffix:
+      result=ind_id.strip()
+    else:
+      result=ind_id.strip()[0:-3]
   else:
     result=ind_id.strip()
   return(result)
@@ -19,61 +23,61 @@ def get_site_id(id: str) -> str:
   result=_remove_suffix(id.strip().split('.')[0])[0:-3]
   return(result)
 
-def get_ind_id(id: str) -> str:
+def get_ind_id(id: str, keep_ss_suffix: bool = False) -> str:
   '''
   This function takes any Pandora_ID and returns the part of it that corresponds to the Individual_ID.
   '''
-  result=_remove_suffix(id.strip().split('.')[0])
+  result=_remove_suffix(id.strip().split('.')[0], keep_ss_suffix)
   return(result)
 
-def get_sample_id(id: str) -> str:
+def get_sample_id(id: str, keep_ss_suffix: bool = False) -> str:
   '''
   This function takes any Pandora_ID and returns the part of it that corresponds to the Sample_ID.
   '''
   x=id.strip().split('.')
   if len(x) < 2:
     raise ValueError('The provided Pandora_ID does not contain the Sample_ID.')
-  result=_remove_suffix(x[0])+'.'+x[1][0]
+  result=_remove_suffix(x[0], keep_ss_suffix)+'.'+x[1][0]
   return(result)
 
-def get_extract_id(id: str) -> str:
+def get_extract_id(id: str, keep_ss_suffix: bool = False) -> str:
   '''
   This function takes any Pandora_ID and returns the part of it that corresponds to the Extract_ID.
   '''
   x=id.strip().split('.')
   if len(x) < 2:
     raise ValueError('The provided Pandora_ID does not contain the Extract_ID.')
-  result=_remove_suffix(x[0])+'.'+x[1][0:3]
+  result=_remove_suffix(x[0], keep_ss_suffix)+'.'+x[1][0:3]
   return(result)
 
-def get_library_id(id: str) -> str:
+def get_library_id(id: str, keep_ss_suffix: bool = False) -> str:
   '''
   This function takes any Pandora_ID and returns the part of it that corresponds to the Library_ID.
   '''
   x=id.strip().split('.')
   if len(x) < 2:
     raise ValueError('The provided Pandora_ID does not contain the Library_ID.')
-  result=_remove_suffix(x[0])+'.'+x[1]
+  result=_remove_suffix(x[0], keep_ss_suffix)+'.'+x[1]
   return(result)
 
-def get_capture_id(id: str) -> str:
+def get_capture_id(id: str, keep_ss_suffix: bool = False) -> str:
   '''
   This function takes any Pandora_ID and returns the part of it that corresponds to the Capture_ID.
   '''
   x=id.strip().split('.')
   if len(x) < 3:
     raise ValueError('The provided Pandora_ID does not contain the Capture_ID.')
-  result=_remove_suffix(x[0])+'.'+x[1]+'.'+x[2]
+  result=_remove_suffix(x[0], keep_ss_suffix)+'.'+x[1]+'.'+x[2]
   return(result)
 
-def get_sequencing_id(id: str) -> str:
+def get_sequencing_id(id: str, keep_ss_suffix: bool = False) -> str:
   '''
   This function takes any Pandora_ID and returns the part of it that corresponds to the Sequencing_ID.
   '''
   x=id.strip().split('.')
   if len(x) < 3:
     raise ValueError('The provided Pandora_ID does not contain the Sequencing_ID.')
-  result=_remove_suffix(x[0])+'.'+x[1]+'.'+x[2]+'.'+x[3]
+  result=_remove_suffix(x[0], keep_ss_suffix)+'.'+x[1]+'.'+x[2]+'.'+x[3]
   return(result)
 
 def test():
@@ -90,19 +94,21 @@ def test():
   print('get_library_id(id) -> str')
   print('get_capture_id(id) -> str')
   print('get_sequencing_id(id) -> str')
-  print('Running test cases...')
   print('')
+  print('Running test cases...')
   
-  for id in ["ABC001", "ABC001.A0101", "ABC001_ss.A0101.SG1.1", "ABCDE001.A0101.SG1.1"]:
+  format_str = '{:15}\t{:25}\t{:25}'
+  for id in ["ABC001", "ABC001.A0101", "ABC001.A0101.SG1.1", "ABCDE001_ss.A0101.SG1.1"]:
     try:
-      print('Pandora_ID:',id)
-      print('Site_ID:',get_site_id(id))
-      print('Individual_ID:',get_ind_id(id))
-      print('Sample_ID:',get_sample_id(id))
-      print('Extract_ID:',get_extract_id(id))
-      print('Library_ID:',get_library_id(id))
-      print('Capture_ID:',get_capture_id(id))
-      print('Sequencing_ID:',get_sequencing_id(id))
+      print(format_str.format(''              ,'suffix=False'                                ,'suffix=True'))
+      print(format_str.format('Pandora_ID:'   , id                                           , id))
+      print(format_str.format('Site_ID:'      , get_site_id(id)                              , get_site_id(id)))
+      print(format_str.format('Individual_ID:', get_ind_id(id, keep_ss_suffix = False)       , get_ind_id(id, keep_ss_suffix = True)))
+      print(format_str.format('Sample_ID:'    , get_sample_id(id, keep_ss_suffix = False)    , get_sample_id(id, keep_ss_suffix = True)))
+      print(format_str.format('Extract_ID:'   , get_extract_id(id, keep_ss_suffix = False)   , get_extract_id(id, keep_ss_suffix = True)))
+      print(format_str.format('Library_ID:'   , get_library_id(id, keep_ss_suffix = False)   , get_library_id(id, keep_ss_suffix = True)))
+      print(format_str.format('Capture_ID:'   , get_capture_id(id, keep_ss_suffix = False)   , get_capture_id(id, keep_ss_suffix = True)))
+      print(format_str.format('Sequencing_ID:', get_sequencing_id(id, keep_ss_suffix = False), get_sequencing_id(id, keep_ss_suffix = True)))
       print('')
     except ValueError as e:
       print(e)
@@ -111,9 +117,18 @@ def test():
 
 def main():
   import argparse
+  
+  ## Custom action class to run test cases without needing a positional argument to be provided
+  def make_action(additional_arg):
+    class customAction(argparse.Action):
+      def __call__(self, parser, args, values, option_string=None):
+        test()
+    return customAction
+
   parser = argparse.ArgumentParser(description='This is a helper module for the pyPandora package. It contains functions that help parse Pandora_IDs.')
-  parser.add_argument('-t','--test', action='store_true', help='Run test cases for the functions in this module. Ignored all other arguments.')
-  parser.add_argument('-v', '--version', action='version', version='%(prog)s '+VERSION)
+  parser.add_argument('-t','--test', nargs=0, action=make_action('test'), help='Run test cases for the functions in this module. Ignored all other arguments.')
+  parser.add_argument('-k','--keep_ss_suffix', action='store_true', help='Keep the ss suffix in the Individual ID (if applicable).')
+  parser.add_argument('-v','--version', action='version', version='%(prog)s '+VERSION)
   parser.add_argument('-g','--get', type=str, help='The function to run. Options: site_id, ind_id, sample_id, extract_id, library_id, capture_id, sequencing_id')
   parser.add_argument('pandora_id', type=str, help='The Pandora_ID to infer from')
   args = parser.parse_args()
